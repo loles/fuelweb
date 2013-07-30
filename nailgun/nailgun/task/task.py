7ca285c4aa646eabe7319391956305a04e6242f1
Łukasz Oleś <lukaszoles@gmail.com>
2013-07-30 23:27:17 +0000

    Include node IP in dict. Required to find SoftLayer node id

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -418,7 +418,8 @@ class DeletionTask(object):
                 nodes_to_delete.append({
                     'id': node.id,
                     'uid': node.id,
-                    'role': node.role
+                    'role': node.role,
+                    'ip' : node.ip
                 })
 
                 if USE_FAKE:
